8476b14905273c5c392044f84c3408c71780988a
Liafell <leneli.lehmann@yahoo.de>
2017-05-23 21:09:43 +0300

    Основная разметка

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-# Webpack-builder
-Сборка Webpack + Yarn
-
-> 1. yarn init -y
-> 2. yarn install
-> 3. yarn add webpack -D
-> 4. yarn run start
+# Accordeon
+Сборка Webpack + Yarn. Используется babel
--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,9 +1,9 @@
 import "./index.scss";
 import "normalize.css";
 
-import createMenu from "../../components/menu/menu";
+import createMenu from "../../components/accordeon/accordeon";
 
-var menu = createMenu(["Главная", "Обо мне", "Портфолио", "Контакты", "Обратная связь"], "menu");
-document.body.appendChild(menu);
+//var menu = createMenu(["Главная", "Обо мне", "Портфолио", "Контакты", "Обратная связь"], "menu");
+//document.body.appendChild(menu);
 
 console.log("in index.js");
