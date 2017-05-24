cccdfbfabc5c8bd26087ce599787fb1d96f85990
Elena <lena@realrussia.co.uk>
2017-05-24 16:47:56 +0300

    Основной функционал

--- source/components/accordeon/accordeon.js
+++ source/components/accordeon/accordeon.js
@@ -1,1 +1,45 @@
 import "./accordeon.scss";
+
+//Elements
+const 	accordeons = document.querySelectorAll(".accordeon");
+
+//Data for Accordeon
+const 	classActive = "active",
+		padding = 20;
+
+
+window.onload = function() { Accordeon.init(); };
+
+
+let Accordeon = {
+	init() {
+		document.addEventListener("click", function(event) {
+			event = event || event.window;
+
+			if(event.target.classList.contains("accordeon__title")) {
+				let activeItem = event.target.parentNode,
+					textItem = activeItem.nextElementSibling,
+					acc = activeItem.parentNode.parentNode;
+
+				Accordeon.toggleClass(activeItem);
+			}
+		});
+	},
+
+	toggleClass(el) {
+		if(el.classList.contains(classActive)) {
+			el.classList.remove(classActive);
+			el.removeAttribute("style");
+		} else {
+			this.height(el);
+			el.classList.add(classActive);
+		}
+	},
+
+	height(el) {
+		let maxHeight = el.scrollHeight + (padding * 2),
+			elHeight  = 0;
+
+		el.style.height = maxHeight + "px";
+	}
+};
--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,9 +1,7 @@
 import "./index.scss";
 import "normalize.css";
 
-import createMenu from "../../components/accordeon/accordeon";
+import accordeon from "../../components/accordeon/accordeon";
 
 //var menu = createMenu(["Главная", "Обо мне", "Портфолио", "Контакты", "Обратная связь"], "menu");
 //document.body.appendChild(menu);
-
-console.log("in index.js");
